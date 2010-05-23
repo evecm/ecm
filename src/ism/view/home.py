39ec7689753fd2535cea36a44900fcef251b4a7a
diabeteman <diab@diabeteman.com>
2010-05-23 02:46:33 +0000

    home page changed

--- src/ism/view/home.py
+++ src/ism/view/home.py
@@ -9,14 +9,13 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template.context import RequestContext
 
-from ism.data.roles.models import MemberDiff, RoleMemberDiff, TitleCompoDiff
+from ism.data.roles.models import MemberDiff, RoleMemberDiff, TitleCompoDiff, Member
 from ism.core.utils import print_time_min
 
 #------------------------------------------------------------------------------
 @login_required
 def home(request):
-    data = {   'last_corped_members' : getLastCorpedMembers(),
-             'last_expelled_members' : getLastExpelledMembers(),
+    data = {               'members' : getLastMembers(),
                     'last_role_adds' : getLastRoleAdds(),
                 'last_role_removals' : getLastRoleRemovals(),
               'last_title_role_adds' : getLastTitleRoleAdds(),
@@ -24,19 +23,39 @@ def home(request):
     
     return render_to_response("home.html", data, context_instance=RequestContext(request))
 
-
 #------------------------------------------------------------------------------
-def getLastCorpedMembers(count=10):
-    members = MemberDiff.objects.filter(new=True).order_by('-id')[:count]
-    for m in members:
-        m.date = print_time_min(m.date)
+def getLastMembers(count=5):
+    members = getLastCorpedMembers(count)
+    members.extend(getLastExpelledMembers(count))
+    members.sort(key=lambda m: m.date, reverse=True)
     return members
 
 #------------------------------------------------------------------------------
-def getLastExpelledMembers(count=10):
-    members = MemberDiff.objects.filter(new=False).order_by('-id')[:count]
-    for m in members:
-        m.date = print_time_min(m.date)
+def getLastCorpedMembers(count=5):
+    queryset = list(MemberDiff.objects.filter(new=True).order_by('-id'))[:count]
+    members = []
+    for m in queryset:
+        if m.new:
+            memb = Member.objects.get(characterID=m.characterID)
+            m.nickname = memb.nickname
+            m.date = print_time_min(memb.corpDate)
+        else:
+            m.date = print_time_min(m.date)
+        members.append(m)
+    return members
+
+#------------------------------------------------------------------------------
+def getLastExpelledMembers(count=5):
+    queryset = list(MemberDiff.objects.filter(new=False).order_by('-id'))[:count]
+    members = []
+    for m in queryset:
+        if m.new:
+            memb = Member.objects.get(characterID=m.characterID)
+            m.nickname = memb.nickname
+            m.date = print_time_min(memb.corpDate)
+        else:
+            m.date = print_time_min(m.date)
+        members.append(m)
     return members
 
 #------------------------------------------------------------------------------
