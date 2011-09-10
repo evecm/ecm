861344ca9a2c8d5aa3cffbb0119fe3db18eed1a9
diabeteman <diab@diabeteman.com>
2011-09-11 01:26:09 +0200

    fixed issue #24

--- src/ecm/view/accounting/wallets.py
+++ src/ecm/view/accounting/wallets.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2010-2011 Robin Jarry
-# 
+#
 # This file is part of EVE Corporation Management.
-# 
-# EVE Corporation Management is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or (at your 
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
-# 
-# EVE Corporation Management is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
+#
+# You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
 
 __date__ = "2011 5 25"
@@ -33,7 +33,7 @@ from ecm.data.accounting.models import JournalEntry
 @check_user_access()
 def list(request):
     data = {
-        'scan_date' : getScanDate(JournalEntry) 
+        'scan_date' : getScanDate(JournalEntry)
     }
     return render_to_response("accounting/wallets.html", data, RequestContext(request))
 
@@ -59,25 +59,25 @@ def list_data(request):
             wallet,
             balance
         ])
-    
+
     if params.column == 0:
         # sort by walletID
         sort_key = lambda e: e[0].walletID
     else:
         # sort by balance
         sort_key = lambda e: e[1]
-    
+
     entries.sort(key=sort_key, reverse=not params.asc)
-    
+
     for wallet in entries:
         wallet[1] = wallet[0].permalink_to_journal(wallet[1])
-        wallet[0] = wallet[0].permalink()
-    
+        wallet[0] = wallet[0].name
+
     json_data = {
         "sEcho" : params.sEcho,
         "iTotalRecords" : total_entries,
         "iTotalDisplayRecords" : filtered_entries,
         "aaData" : entries
     }
-    
+
     return HttpResponse(json.dumps(json_data))
