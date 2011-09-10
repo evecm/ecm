dbe26894b538972f4cd94a25a722943559e67e4d
diabeteman <diab@diabeteman.com>
2011-09-11 01:42:58 +0200

    created django template_filters.py and modified some code to use the filters

--- src/ecm/view/members/details.py
+++ src/ecm/view/members/details.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2010-2011 Robin Jarry
-# 
+#
 # This file is part of EVE Corporation Management.
-# 
-# EVE Corporation Management is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or (at your 
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
-# 
-# EVE Corporation Management is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
+#
+# You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
 
 __date__ = "2011-03-13"
@@ -27,7 +27,6 @@ from django.template.context import RequestContext
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.core.exceptions import ObjectDoesNotExist
 
-from ecm.core import utils
 from ecm.data.roles.models import MemberDiff, Member, RoleMemberDiff, TitleMemberDiff
 from ecm.view import getScanDate, extract_datatable_params
 from ecm.view.decorators import check_user_access
@@ -35,6 +34,8 @@ from ecm.data.common.models import ColorThreshold
 from ecm.core.utils import print_time_min, get_access_color
 from ecm.core.eve import db
 
+import logging
+logger = logging.getLogger(__name__)
 
 #------------------------------------------------------------------------------
 @check_user_access()
@@ -42,23 +43,18 @@ def details(request, characterID):
     try:
         colorThresholds = ColorThreshold.objects.all().order_by("threshold")
         member = Member.objects.get(characterID=int(characterID))
-        member.corpDate = print_time_min(member.corpDate)
-        member.lastLogin = print_time_min(member.lastLogin)
-        member.lastLogoff = print_time_min(member.lastLogoff)
         member.base = db.resolveLocationName(member.baseID)[0]
         member.color = get_access_color(member.accessLvl, colorThresholds)
         member.roles_no_director = member.roles.exclude(roleID=1) # exclude 'director'
-        member.all_titles = member.titles.all()
-        member.is_director = member.is_director()
-        
+
         if member.corped:
             member.date = getScanDate(Member)
         else:
             d = MemberDiff.objects.filter(member=member, new=False).order_by("-id")[0]
-            member.date = utils.print_time_min(d.date)
+            member.date = d.date
     except ObjectDoesNotExist:
         member = Member(characterID=int(characterID), name="???")
-    
+
     data = { 'member' : member }
     return render_to_response("members/member_details.html", data, RequestContext(request))
 
@@ -70,10 +66,13 @@ def update_member_notes(request, characterID):
         member = get_object_or_404(Member, characterID=int(characterID))
         member.notes = new_notes
         member.save()
+
+        logger.info('"%s" edited notes on "%s" : %s' % (request.user, member, new_notes))
+
         return HttpResponse(new_notes)
     except:
         return HttpResponseBadRequest()
-    
+
 
 #------------------------------------------------------------------------------
 @check_user_access()
@@ -83,29 +82,29 @@ def access_changes_member_data(request, characterID):
         params = extract_datatable_params(request)
     except:
         return HttpResponseBadRequest()
-    
+
     roles = RoleMemberDiff.objects.filter(member=characterID).order_by("-id")
     titles = TitleMemberDiff.objects.filter(member=characterID).order_by("-id")
-    
+
     count = roles.count() + titles.count()
-    
+
     changes = list(roles) + list(titles)
     changes.sort(key=lambda e: e.date, reverse=True)
     changes = changes[params.first_id:params.last_id]
-    
+
     change_list = []
     for change in changes:
         change_list.append([
             change.new,
-            change.access_permalink(),
+            change.access_permalink,
             print_time_min(change.date)
         ])
-    
+
     json_data = {
         "sEcho" : params.sEcho,
         "iTotalRecords" : count,
         "iTotalDisplayRecords" : count,
         "aaData" : change_list
     }
-    
+
     return HttpResponse(json.dumps(json_data))
--- src/ecm/view/titles/details.py
+++ src/ecm/view/titles/details.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2010-2011 Robin Jarry
-# 
+#
 # This file is part of EVE Corporation Management.
-# 
-# EVE Corporation Management is free software: you can redistribute it and/or 
-# modify it under the terms of the GNU General Public License as published by 
-# the Free Software Foundation, either version 3 of the License, or (at your 
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
-# 
-# EVE Corporation Management is distributed in the hope that it will be useful, 
-# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
+#
+# You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
 
 __date__ = "2011-03-13"
@@ -37,19 +37,19 @@ from ecm.core.utils import get_access_color
 #------------------------------------------------------------------------------
 @check_user_access()
 def details(request, id):
-    
+
     title = get_object_or_404(Title, titleID=int(id))
     title.lastModified = TitleCompoDiff.objects.filter(title=title).order_by("-id")
     if title.lastModified:
-        title.lastModified = utils.print_time_min(title.lastModified[0].date)
+        title.lastModified = title.lastModified[0].date
     else:
         title.lastModified = None
     title.color = get_access_color(title.accessLvl, ColorThreshold.objects.all().order_by("threshold"))
 
     data = {
         "title" : title,
-        "member_count" : title.members.count(),  
-        "colorThresholds" : ColorThreshold.as_json() 
+        "member_count" : title.members.count(),
+        "colorThresholds" : ColorThreshold.as_json()
     }
 
     return render_to_response("titles/title_details.html", data, RequestContext(request))
@@ -68,31 +68,31 @@ def composition_data(request, id):
 
     title = get_object_or_404(Title, titleID=int(id))
     query = TitleComposition.objects.filter(title=title)
-    
-    if params.asc: 
+
+    if params.asc:
         query = query.order_by("role")
     else:
         query = query.order_by("-role")
-    
+
     total_compos = query.count()
-    
+
     query = query[params.first_id:params.last_id]
-    
+
     compo_list = []
     for compo in query:
         compo_list.append([
-            compo.role.permalink(),
-            compo.role.roleType.permalink(),
+            compo.role.permalink,
+            compo.role.roleType.permalink,
             compo.role.get_access_lvl()
         ])
-    
+
     json_data = {
         "sEcho" : params.sEcho,
         "iTotalRecords" : total_compos,
         "iTotalDisplayRecords" : total_compos,
         "aaData" : compo_list
     }
-    
+
     return HttpResponse(json.dumps(json_data))
 
 
@@ -108,23 +108,23 @@ def compo_diff_data(request, id):
     title = get_object_or_404(Title, titleID=int(id))
     query = TitleCompoDiff.objects.filter(title=title).order_by("-date")
     total_diffs = query.count()
-    
+
     query = query[params.first_id:params.last_id]
-    
+
     diff_list = []
     for diff in query:
         diff_list.append([
             diff.new,
-            diff.role.permalink(),
-            diff.role.roleType.permalink(),
+            diff.role.permalink,
+            diff.role.roleType.permalink,
             utils.print_time_min(diff.date)
         ])
-    
+
     json_data = {
         "sEcho" : params.sEcho,
         "iTotalRecords" : total_diffs,
         "iTotalDisplayRecords" : total_diffs,
         "aaData" : diff_list
     }
-    
+
     return HttpResponse(json.dumps(json_data))
