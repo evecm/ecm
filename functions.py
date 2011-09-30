54a9bf49d0c48b5ff6cdaa66177b73e87d61ca12
diabeteman <diab@diabeteman.com>
2011-10-01 01:55:59 +0200

    forgot one __future__ import...

--- functions.py
+++ functions.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import with_statement
+
 import sys
 from os import path
 import fnmatch
