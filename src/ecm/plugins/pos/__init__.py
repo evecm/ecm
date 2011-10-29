971ce0ad35db0b012011aa3b7d1d9b198d79b1f0
Jerrykhan <devnull@localhost>
2011-10-29 18:55:18 +0200

    Migration to the plugin POS of the POS dev branche,

---
+++ src/ecm/plugins/pos/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2011 jerome Vacher
+#
+# This file is part of EVE Corporation Management.
+#
+# EVE Corporation Management is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# EVE Corporation Management is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
+
+
+NAME = 'pos'
+
+DEPENDS_ON = {
+    'common' : (2,),
+    'corp' : (2,),
+    'hr' : (2,),
+    'scheduler' : (2,),
+}
+
+MENUS = [
+     {'menu_title': 'POSes',    'menu_url': '',      'menu_items': None},
+]
+
+TASKS = [
+    {
+        'function' : 'ecm.plugins.pos.tasks.reftypes.update',
+        'priority' : 100,
+        'frequency' : 7,
+        'frequency_units' : 43200, # twice a day
+    },
+]
+
+URL_PERMISSIONS = [
+    r'^/pos.*$',
+]
