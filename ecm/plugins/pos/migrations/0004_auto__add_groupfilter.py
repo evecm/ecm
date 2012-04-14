a6a0ca22620aa2dc7fda0a609e496c7f1bdccf11
Tash Rockski <tashrockski@gmail.com>
2012-04-14 19:21:39 +0200

    POS: Feature #95 migrations

---
+++ ecm/plugins/pos/migrations/0004_auto__add_groupfilter.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'GroupFilter'
+        db.create_table('pos_groupfilter', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('group_name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('pos', self.gf('django.db.models.fields.related.ForeignKey')(related_name='group_filter', to=orm['pos.POS'])),
+        ))
+        db.send_create_signal('pos', ['GroupFilter'])
+
+    def backwards(self, orm):
+        # Deleting model 'GroupFilter'
+        db.delete_table('pos_groupfilter')
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pos.fuellevel': {
+            'Meta': {'ordering': "['pos', 'date', 'type_id']", 'object_name': 'FuelLevel'},
+            'consumption': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pos': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fuel_levels'", 'to': "orm['pos.POS']"}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {}),
+            'type_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'})
+        },
+        'pos.groupfilter': {
+            'Meta': {'object_name': 'GroupFilter'},
+            'group_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pos': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'group_filter'", 'to': "orm['pos.POS']"})
+        },
+        'pos.pos': {
+            'Meta': {'object_name': 'POS'},
+            'allow_alliance_members': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_corporation_members': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_aggression': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_concord_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_corp_war': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'cached_until': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'custom_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'deploy_flags': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'fuel_type_id': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'has_sov': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'item_id': ('django.db.models.fields.BigIntegerField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'location_id': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'moon': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'moon_id': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'online_timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'operators': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'operated_poses'", 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'security_status_threshold': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'standings_threshold': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'state': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'state_timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'type_id': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'type_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'usage_flags': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'use_standings_from': ('django.db.models.fields.BigIntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['pos']
---
+++ ecm/plugins/pos/migrations/0007_auto__del_field_groupfilter_pos__add_field_groupfilter_group.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'GroupFilter.pos'
+        db.delete_column('pos_groupfilter', 'pos_id')
+
+        # Adding field 'GroupFilter.group'
+        db.add_column('pos_groupfilter', 'group',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='group', null=True, to=orm['auth.Group']),
+                      keep_default=False)
+
+        # Removing M2M table for field group on 'GroupFilter'
+        db.delete_table('pos_groupfilter_group')
+
+        # Adding M2M table for field pos on 'GroupFilter'
+        db.create_table('pos_groupfilter_pos', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('groupfilter', models.ForeignKey(orm['pos.groupfilter'], null=False)),
+            ('pos', models.ForeignKey(orm['pos.pos'], null=False))
+        ))
+        db.create_unique('pos_groupfilter_pos', ['groupfilter_id', 'pos_id'])
+
+    def backwards(self, orm):
+
+        # User chose to not deal with backwards NULL issues for 'GroupFilter.pos'
+        raise RuntimeError("Cannot reverse this migration. 'GroupFilter.pos' and its values cannot be restored.")
+        # Deleting field 'GroupFilter.group'
+        db.delete_column('pos_groupfilter', 'group_id')
+
+        # Adding M2M table for field group on 'GroupFilter'
+        db.create_table('pos_groupfilter_group', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('groupfilter', models.ForeignKey(orm['pos.groupfilter'], null=False)),
+            ('group', models.ForeignKey(orm['auth.group'], null=False))
+        ))
+        db.create_unique('pos_groupfilter_group', ['groupfilter_id', 'group_id'])
+
+        # Removing M2M table for field pos on 'GroupFilter'
+        db.delete_table('pos_groupfilter_pos')
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pos.fuellevel': {
+            'Meta': {'ordering': "['pos', 'date', 'type_id']", 'object_name': 'FuelLevel'},
+            'consumption': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pos': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fuel_levels'", 'to': "orm['pos.POS']"}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {}),
+            'type_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'})
+        },
+        'pos.groupfilter': {
+            'Meta': {'object_name': 'GroupFilter'},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'group'", 'null': 'True', 'to': "orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pos': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'group_filter'", 'null': 'True', 'symmetrical': 'False', 'to': "orm['pos.POS']"})
+        },
+        'pos.pos': {
+            'Meta': {'object_name': 'POS'},
+            'allow_alliance_members': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'allow_corporation_members': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_aggression': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_concord_flag': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'attack_on_corp_war': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'cached_until': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'custom_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'deploy_flags': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'fuel_type_id': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'has_sov': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'item_id': ('django.db.models.fields.BigIntegerField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'location_id': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'moon': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'moon_id': ('django.db.models.fields.BigIntegerField', [], {'default': '0'}),
+            'notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'online_timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'operators': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'operated_poses'", 'symmetrical': 'False', 'to': "orm['auth.User']"}),
+            'security_status_threshold': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'standings_threshold': ('django.db.models.fields.FloatField', [], {'default': '0.0'}),
+            'state': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'state_timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'type_id': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'type_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'usage_flags': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'use_standings_from': ('django.db.models.fields.BigIntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['pos']
