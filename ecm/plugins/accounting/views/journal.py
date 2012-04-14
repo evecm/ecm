40c8d50dddc3ce88d547e6783e775356e7c53cb9
Tash Rockski <tashrockski@gmail.com>
2012-04-14 23:27:56 +0200

    Accounting: Feature #78 Filtering enhancements for transactions

--- ecm/plugins/accounting/views/journal.py
+++ ecm/plugins/accounting/views/journal.py
@@ -14,6 +14,9 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
+import datetime
+from django.db.models.aggregates import Min, Max
+import operator
 
 __date__ = "2011 5 23"
 __author__ = "diabeteman"
@@ -24,6 +27,8 @@ try:
 except ImportError:
     # fallback for python 2.5
     import django.utils.simplejson as json
+    
+from datetime import datetime, timedelta
 
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.shortcuts import render_to_response
@@ -39,14 +44,24 @@ from ecm.apps.hr.models import Member
 from ecm.views.decorators import check_user_access
 from ecm.views import extract_datatable_params
 from ecm.plugins.accounting.models import JournalEntry, EntryType
+import logging
 
+LOG = logging.getLogger(__name__)
+DATE_PATTERN = "%Y-%m-%d"
 
 #------------------------------------------------------------------------------
 @check_user_access()
 def journal(request):
     walletID = int(request.GET.get('walletID', 0))
     entryTypeID = int(request.GET.get('entryTypeID', 0))
-
+    amount = request.GET.get('amount',None)
+    comparator = request.GET.get('comparator','>')
+    
+    from_date = JournalEntry.objects.all().aggregate(date=Min("date"))["date"]
+    if from_date is None: from_date = datetime.fromtimestamp(0)
+    to_date = JournalEntry.objects.all().aggregate(date=Max("date"))["date"]
+    if to_date is None: to_date = datetime.now()
+    
     wallets = [{ 'walletID' : 0, 'name' : 'All', 'selected' : walletID == 0 }]
     for w in Wallet.objects.all().order_by('walletID'):
         wallets.append({
@@ -66,7 +81,11 @@ def journal(request):
     data = {
         'wallets' : wallets,
         'entryTypes' : entryTypes,
-        'scan_date' : UpdateDate.get_latest(JournalEntry)
+        'amount' : amount,
+        'comparator' : comparator,
+        'scan_date' : UpdateDate.get_latest(JournalEntry),
+        'from_date' : datetime.strftime(from_date, DATE_PATTERN),
+        'to_date' : datetime.strftime(to_date, DATE_PATTERN),
     }
     return render_to_response("wallet_journal.html", data, Ctx(request))
 
@@ -75,6 +94,7 @@ def journal(request):
 
 #------------------------------------------------------------------------------
 journal_cols = ['wallet', 'date', 'type', 'ownerName1', 'ownerName2', 'amount', 'balance']
+
 @check_user_access()
 def journal_data(request):
     try:
@@ -82,12 +102,16 @@ def journal_data(request):
         REQ = request.GET if request.method == 'GET' else request.POST
         params.walletID = int(REQ.get('walletID', 0))
         params.entryTypeID = int(REQ.get('entryTypeID', 0))
+        params.amount = request.GET.get('amount',None)
+        params.comparator = request.GET.get('comparator','>')
+        params.from_date = datetime.strptime(REQ.get('from_date', None), DATE_PATTERN)
+        params.to_date = datetime.strptime(REQ.get('to_date', None), DATE_PATTERN)
     except:
         return HttpResponseBadRequest()
 
     query = JournalEntry.objects.select_related(depth=1).all().order_by('-date')
 
-    if params.search or params.walletID or params.entryTypeID:
+    if params.search or params.walletID or params.entryTypeID or params.amount or (params.from_date and params.to_date):
         total_entries = query.count()
         search_args = Q()
 
@@ -102,7 +126,25 @@ def journal_data(request):
             search_args &= Q(wallet=params.walletID)
         if params.entryTypeID:
             search_args &= Q(type=params.entryTypeID)
-
+        # Add query amount
+        LOG.debug(params.amount)
+        if params.amount:
+            comparator_map = {
+                              '>':  Q(amount__gt=params.amount), 
+                              '<':  Q(amount__lt=params.amount), 
+                              '>=': Q(amount__gte=params.amount),
+                              '<=': Q(amount__lte=params.amount),
+                              '==': Q(amount=params.amount),
+                              '<>': Q(amount__lt=params.amount, amount__gt=params.amount),
+                              }
+            search_args &= comparator_map[params.comparator]
+            LOG.debug(comparator_map[params.comparator])
+            
+        # Add query dates
+        if params.from_date and params.to_date:
+            # + 24 hours on the to date
+            search_args &= Q(date__range=(params.from_date, params.to_date + timedelta(days=1)))
+            
         query = query.filter(search_args)
         filtered_entries = query.count()
     else:
