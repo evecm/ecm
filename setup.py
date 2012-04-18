6301acfc1d3fd4a7f9deeb5f2e9be9787883ff09
diabeteman <diab@diabeteman.com>
2012-04-18 23:27:52 +0200

    Fixed installation (package discovery was bugged)

--- setup.py
+++ setup.py
@@ -16,35 +16,14 @@
 # You should have received a copy of the GNU General Public License along with
 # EVE Corporation Management. If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from distutils.command.install import INSTALL_SCHEMES
-import os
 
 # Tell distutils not to put the data_files in platform-specific installation
 # locations. See here for an explanation:
 # http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-def find_packages(root):
-    packages = []
-    for dirpath, _, filenames in os.walk(root):
-        if '__init__.py' in filenames:
-            packages.append('.'.join(fullsplit(dirpath)))
 
 dependencies = [
     'django_simple_captcha (>= 0.3)',
@@ -85,7 +64,7 @@ setup(
     install_requires = [ dep.split()[0] for dep in dependencies ],
 
     # CONTENTS
-    packages = find_packages('ecm'),
+    packages = find_packages(),
     include_package_data = True,
     zip_safe = False,
     entry_points = {
