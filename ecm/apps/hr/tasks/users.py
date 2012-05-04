c72d1cf84b2d61a647d0a9ac0fe87cd4ef1b2991
ajurna <ajurna@gmail.com>
2012-05-04 17:35:24 +0100

    proposed fix for bug #140

--- ecm/apps/hr/tasks/users.py
+++ ecm/apps/hr/tasks/users.py
@@ -23,6 +23,8 @@ import logging
 from django.db import transaction
 
 from django.contrib.auth.models import User, Group
+from django.conf import settings
+from django.db.models import Max
 
 from ecm.apps.eve import api
 from ecm.lib import eveapi
@@ -103,10 +105,14 @@ def update_all_users_accesses():
                                  "Skipping user access update.")
     except ScheduledTask.DoesNotExist:
         pass
-
+    #refernce: http://stackoverflow.com/questions/5342440/reset-auto-increment-counter-in-postgres
+    if 'postgresql' in settings.DATABASES['default']['ENGINE']:
+        maxid = Group.objects.all().aggregate(Max('id'))['id__max'] + 1
+        Group.objects.raw('ALTER SEQUENCE auth_group_id_seq RESTART WITH %s;', (maxid))
+        
     corp_members_group = get_members_group()
     directors_group = get_directors_group()
-
+    
     LOG.info("Updating user accesses from their in-game roles...")
     for user in User.objects.filter(is_active=True):
         update_user_accesses(user, corp_members_group, directors_group)
