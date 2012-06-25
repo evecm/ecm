2a273bc78a9d5f0b389a0d3160229eddc99cf998
diabeteman <diab@diabeteman.com>
2012-06-26 00:58:23 +0200

    Changed {{STATIC_URL}} by {% static %} template tags

--- ecm/admin/instance_template/settings.py
+++ ecm/admin/instance_template/settings.py
@@ -166,8 +166,13 @@ TEMPLATE_DIRS = (
     rel_path('templates/', root=ECM_PACKAGE),
 )
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
     'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
 
     'ecm.views.context_processors.corporation_name',
     'ecm.views.context_processors.menu',
