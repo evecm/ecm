6a41d8eba7db831dfb6316888837f08e0274f0ff
Azerith <devnull@localhost>
2012-07-23 19:58:47 +1000

    added wallet_journal.js fixes issue#220

---
+++ ecm/plugins/accounting/templates/ecm/accounting/wallet_journal.js
@@ -0,0 +1,32 @@
+/*************************
+ * Wallet journal JS file
+ *
+ * 
+ * ***********************/
+
+function walletJournalRowCallback(nRow, aData, iDisplayIndex, iDisplayIndexFull){
+    if (aData[5].charAt(0) == '+') {
+        $('td:eq(5)', nRow).addClass('journal-credit');
+    } else {
+        $('td:eq(5)', nRow).addClass('journal-dwbit');
+    }
+    /* hide reason column */
+    $('td:eq(7)', nRow).hide()
+
+    /* set titles tooltip on each row */
+    reason = aData[7]
+    if (reason != "") {
+        $('td:eq(5)', nRow).attr("title", reason)
+        $('td:eq(5)', nRow).cluetip({
+            splitTitle: '|',
+            dropShadow: false,
+            cluetipClass: 'jtip',
+            positionBy: 'mouse',
+            tracking: true
+        });
+    }
+
+    return nRow;
+}
+
+
