2f251db1da6f45c6e2d88beb8b01f554bc36a094
Drezil <devnull@localhost>
2012-07-25 00:55:51 +0200

    removed some old code, cleaned up

--- ecm/apps/hr/templates/ecm/hr/players_detail_datatable.js
+++ ecm/apps/hr/templates/ecm/hr/players_detail_datatable.js
@@ -35,33 +35,3 @@ function playerDetailRowCallback( nRow, aData, iDisplayIndex, iDisplayIndexFull 
 
     return nRow;
 }
-        
-//$(document).ready(function () {
-//	
-//	  /* trigger the search when pressing return in the text field */
-//    $("#search_form").submit(function(event) {
-//        event.preventDefault();
-//        $('#players_table').fnFilter($("#search_text").val());
-//    });
-//
-//    /* trigger the search when clicking the "search" button */
-//    $("#search_button").click(function() {
-//    	$('#players_table').fnFilter($("#search_text").val());
-//    });
-//
-//    /* reset the search when clicking the "reset" button */
-//    $("#clear_search").click(function() {
-//        $("#search_text").val("");
-//        $('#players_table').fnFilter("");
-//    });
-//
-//
-//    /* disable multi column sorting */
-//    $('#players_table thead th').click(function(event) {
-//        if (!$(event.target).hasClass('sorthandle')) {
-//            event.shiftKey = false;
-//        }
-//    });
-//    
-//});
-
