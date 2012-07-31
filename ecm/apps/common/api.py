2ed0a4143b45683b38c017e7acd7a31a8ce6d722
Drezil <devnull@localhost>
2012-07-31 20:26:12 +0200

    fixed indention in file. Use SPACEs instead of TABs ..

--- ecm/apps/common/api.py
+++ ecm/apps/common/api.py
@@ -115,10 +115,11 @@ class Character:
 
 def get_account_characters(user_api):
     connection = connect_user(user_api)
-	try:
-		response = connection.account.APIKeyInfo()
-	except eveapi.Error:
-		return []
+    response = None
+    try:
+        response = connection.account.APIKeyInfo()
+    except eveapi.Error:
+        return []
     corp = Corp.objects.get(id=1)
     characters = []
     if response.key.type.lower() != "account":
