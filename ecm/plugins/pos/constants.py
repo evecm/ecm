c0ea2cb8d4fe8f208f821b5fb53e5b354b86b68b
ajurna <ajurna@gmail.com>
2013-04-04 22:48:02 +0100

    fix for bug #349, now calculates to full for all types of reactions.

--- ecm/plugins/pos/constants.py
+++ ecm/plugins/pos/constants.py
@@ -63,3 +63,70 @@ RACEID_TO_CHARTERID = {
     500007: AMMATAR_MANDATE_STARBASE_CHARTER,
     500008: KHANID_KINGDOM_STARBASE_CHARTER,
 }
+
+COMPLEX_REACTIONS = {
+    16670: 10000,       #Crystalline Carbonide Reaction
+    17317: 200,         #Fermionic Condensates Reaction
+    16673: 10000,       #Fernite Carbide Reaction
+    16683: 400,         #Ferrogel Reaction
+    16679: 3000,        #Fulleride Reaction
+    16682: 750,         #Hypersynaptic Fibers Reaction
+    16681: 1500,        #Nanotransistors Reaction
+    16680: 2200,        #Phenolic Composites Reaction
+    16678: 6000,        #Sylramic Fibers Reaction
+    16671: 10000,       #Titanium Carbide Reaction
+    16672: 10000,       #Tungsten Carbide Reaction
+}
+SIMPLE_REACTIONS = {
+    16663: 200,
+    16659: 200,
+    16660: 200,
+    16655: 200,
+    16668: 200,
+    16656: 200,
+    16669: 200,
+    17769: 200,
+    16665: 200,
+    16666: 200,
+    16667: 200,
+    16662: 200,
+    17960: 200,
+    16657: 200,
+    16658: 200,
+    16664: 200,
+    16661: 200,
+    16654: 200,
+    17959: 200,
+}
+UNREFINED = [
+    29640,
+    29641,
+    29642,
+    29643,
+    29644,
+    29645,
+    29659,
+    29660,
+    29661,
+    29662,
+    29663,
+    29664,
+    32821,
+    32822,
+    32823,
+    32824,
+    32825,
+    32826,
+    32827,
+    32828,
+    32829,
+    32830,
+    32831,
+    32832,
+    32833,
+    32834,
+    32835,
+    32836,
+    32837,
+    32838,
+]
