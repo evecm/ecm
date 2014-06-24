dbe382d93332c142dcf06a709b51ee516ff063ba
ggrog <tim@ggrog.com>
2014-06-23 23:21:55 -0500

    Ensure mercenary wallet fix gets inserted into the database (overkill?)

--- ecm/apps/corp/migrations/0009_mercenary_wallet_data.py
+++ ecm/apps/corp/migrations/0009_mercenary_wallet_data.py
@@ -7,10 +7,8 @@ from django.db import models
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName". 
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
+        from django.core.management import call_command
+        call_command("loaddata", "0009_mercenary_wallet_data.json")
 
     def backwards(self, orm):
         "Write your backwards methods here."
